a188f4daa9332cc3f1f5826ac53013112f427f0f
seryuk <alexseryukov@mail.ru>
2024-08-30 23:00:24 +0300

    add_cvar_result

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 from models.cvar import CvarRequest, CvarResult
+
+
 app = FastAPI()
+
+
 @app.post("/")
 async def cvar_request(cvar: CvarRequest)->CvarResult:
     result = CvarResult(cvar = 3)
     return result
+
 
 if __name__ == "__main__":
     import uvicorn
